3eeb3d7c6473c74ed7ec3748c72fd78672da7566
Dan Stoian <danmihaistoian@gmail.com>
2023-11-02 19:45:16 +0200

    Initial Commit

---
+++ Howdy/HowdyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowdyApp.swift
+//  Howdy
+//
+//  Created by Dan Stoian on 02.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct HowdyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
